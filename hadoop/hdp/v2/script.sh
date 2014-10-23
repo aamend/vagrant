711abfa1869879f98e11886fdf82f23530566660
Antoine Amend <antoine.amend@kpmg.co.uk>
2014-10-23 11:37:37 +0100

    Add Hive, and start Spark as Local mode

--- hadoop/hdp/v2/script.sh
+++ hadoop/hdp/v2/script.sh
@@ -9,31 +9,26 @@
 VAGRANT_ETH1_IP=`ifconfig eth1 | grep 'inet' | grep -v '127.0.0.1' | grep -v 'inet6' | cut -d: -f2 | awk '{print $1}'`
 VAGRANT_HOST=`hostname`
 
-if [ ! -e /hadoop ] ; then
-  sudo mkdir /hadoop
-fi
-
 echo "**************************************"
 echo "Install common packages"
 echo "**************************************"
 
-sudo apt-get install -y unzip
-sudo apt-get install -y software-properties-common
-sudo apt-get install -y apt-file
-sudo apt-get install -y vim
-sudo apt-get install -y python-software-properties
-sudo apt-get install -y curl
-sudo apt-get install -y wget
+apt-get install -y unzip
+apt-get install -y software-properties-common
+apt-get install -y apt-file
+apt-get install -y vim
+apt-get install -y python-software-properties
+apt-get install -y curl
+apt-get install -y wget
 
 echo "**************************************"
-echo "Install Cloudera repository (for Java)"
+echo "Install SSH key"
 echo "**************************************"
 
-cat > /etc/apt/sources.list.d/cloudera-cm.list << EOF
-deb [arch=amd64] http://archive.cloudera.com/cm4/ubuntu/precise/amd64/cm precise-cm4 contrib
-deb-src http://archive.cloudera.com/cm4/ubuntu/precise/amd64/cm precise-cm4 contrib
-EOF
-curl -s http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh/archive.key | sudo apt-key add -
+ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
+chown vagrant:vagrant /root/.ssh/id_rsa
+chown vagrant:vagrant /root/.ssh/id_rsa.pub
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 echo "**************************************"
 echo "Install Hortonworks repository"
@@ -48,33 +43,31 @@ echo "Install Hadoop packages"
 echo "**************************************"
 
 wget http://www.magnatempusgroup.net/ftphost/releases/MTG-0.3.1/ubuntu/pool/contrib/b/bigtop-utils/bigtop-utils_0.3.1.MTG-1_all.deb
-sudo dpkg -i bigtop-utils_0.3.1.MTG-1_all.deb
-sudo rm bigtop-utils_0.3.1.MTG-1_all.deb
+dpkg -i bigtop-utils_0.3.1.MTG-1_all.deb
+rm bigtop-utils_0.3.1.MTG-1_all.deb
 
-sudo apt-get update
-sudo apt-get install -y oracle-j2sdk1.6
-sudo apt-get install -y hadoop-hdfs-namenode
-sudo apt-get install -y hadoop-hdfs-datanode
-sudo apt-get install -y hadoop-yarn-resourcemanager
-sudo apt-get install -y hadoop-yarn-nodemanager
-sudo apt-get install -y hadoop-mapreduce-historyserver
-sudo apt-get install -y hadoop-yarn-proxyserver
+apt-get update
+apt-get install -y openjdk-7-jdk
+apt-get install -y openjdk-7-jre-headless
+apt-get install -y hadoop-hdfs-namenode
+apt-get install -y hadoop-hdfs-datanode
+apt-get install -y hadoop-yarn-resourcemanager
+apt-get install -y hadoop-yarn-nodemanager
+apt-get install -y hadoop-mapreduce-historyserver
+apt-get install -y hadoop-yarn-proxyserver
 
-VARIABLE_SET=`cat /etc/environment | grep JAVA_HOME | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "JAVA_HOME=/usr/lib/jvm/j2sdk1.6-oracle" >> /etc/environment
-fi
-export JAVA_HOME=/usr/lib/jvm/j2sdk1.6-oracle
+echo "JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64" >> /etc/environment
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
 echo "**************************************"
 echo "Create Hadoop disk /hadoop"
 echo "**************************************"
 
-sudo mkdir -p /hadoop/dfs/{nn,dn}
-sudo chown -R hdfs:hdfs /hadoop/dfs
-sudo chmod 700 /hadoop/dfs
-sudo mkdir -p /hadoop/yarn/{local,logs}
-sudo chown -R yarn:yarn /hadoop/yarn
+mkdir -p /hadoop/dfs/{nn,dn}
+chown -R hdfs:hdfs /hadoop/dfs
+chmod 700 /hadoop/dfs
+mkdir -p /hadoop/yarn/{local,logs}
+chown -R yarn:yarn /hadoop/yarn
 
 echo "**************************************"
 echo "Set Hadoop configuration"
@@ -94,186 +87,267 @@ EOF
 
 echo ${VAGRANT_ETH1_IP} > /etc/hadoop/conf/slaves
 
-sudo cp /vagrant/tmpl/core-site.xml /etc/hadoop/conf/core-site.xml
-sudo sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/core-site.xml
-sudo sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/core-site.xml
+cp /vagrant/tmpl/core-site.xml /etc/hadoop/conf/core-site.xml
+sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/core-site.xml
+sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/core-site.xml
 cat /etc/hadoop/conf/core-site.xml
 
-sudo cp /vagrant/tmpl/hdfs-site.xml /etc/hadoop/conf/hdfs-site.xml
-sudo sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/hdfs-site.xml
-sudo sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/hdfs-site.xml
+cp /vagrant/tmpl/hdfs-site.xml /etc/hadoop/conf/hdfs-site.xml
+sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/hdfs-site.xml
+sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/hdfs-site.xml
 cat /etc/hadoop/conf/hdfs-site.xml
 
-sudo cp /vagrant/tmpl/mapred-site.xml /etc/hadoop/conf/mapred-site.xml
-sudo sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/mapred-site.xml
-sudo sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/mapred-site.xml
+cp /vagrant/tmpl/mapred-site.xml /etc/hadoop/conf/mapred-site.xml
+sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/mapred-site.xml
+sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/mapred-site.xml
 cat /etc/hadoop/conf/mapred-site.xml
 
-sudo cp /vagrant/tmpl/yarn-site.xml /etc/hadoop/conf/yarn-site.xml
-sudo sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/yarn-site.xml
-sudo sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/yarn-site.xml
+cp /vagrant/tmpl/yarn-site.xml /etc/hadoop/conf/yarn-site.xml
+sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hadoop/conf/yarn-site.xml
+sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hadoop/conf/yarn-site.xml
 cat /etc/hadoop/conf/yarn-site.xml
 
-if [ ! -e /hadoop/setup-hdfs ] ; then
+echo "**************************************"
+echo "Format HDFS"
+echo "**************************************"
 
-  echo "**************************************"
-  echo "Format HDFS"
-  echo "**************************************"
-
-  sudo -u hdfs hdfs namenode -format
-  touch /hadoop/setup-hdfs
-fi
+-u hdfs hdfs namenode -format
 
 echo "**************************************"
 echo "Starting HDFS"
 echo "**************************************"
 
-sudo service hadoop-hdfs-namenode start
-sudo service hadoop-hdfs-datanode start
+service hadoop-hdfs-namenode start
+service hadoop-hdfs-datanode start
 
 echo "**************************************"
 echo "Creating HDFS structure"
 echo "**************************************"
 
-sudo -u hdfs hadoop fs -mkdir /tmp 
-sudo -u hdfs hadoop fs -mkdir /user
-sudo -u hdfs hadoop fs -mkdir /user/vagrant
-sudo -u hdfs hadoop fs -mkdir /user/history
-sudo -u hdfs hadoop fs -mkdir /var/log/hadoop-yarn
-sudo -u hdfs hadoop fs -chmod -R 1777 /tmp
-sudo -u hdfs hadoop fs -chmod -R 1777 /user/history
-sudo -u hdfs hadoop fs -chown yarn:mapred /var/log/hadoop-yarn
-sudo -u hdfs hadoop fs -chown vagrant /user/vagrant
-sudo -u hdfs hadoop fs -chown yarn /user/history
-sudo -u hdfs hadoop fs -ls -R /
+-u hdfs hadoop fs -mkdir /tmp 
+-u hdfs hadoop fs -mkdir /user
+-u hdfs hadoop fs -mkdir /var
+-u hdfs hadoop fs -mkdir /user/vagrant
+-u hdfs hadoop fs -mkdir /user/history
+-u hdfs hadoop fs -mkdir /var/log
+-u hdfs hadoop fs -mkdir /var/log/hadoop-yarn
+
+-u hdfs hadoop fs -chmod -R 1777 /tmp
+-u hdfs hadoop fs -chmod -R 1777 /user/history
+-u hdfs hadoop fs -chown -R yarn:mapred /var/log/hadoop-yarn
+-u hdfs hadoop fs -chown -R vagrant /user/vagrant
+-u hdfs hadoop fs -chown -R yarn /user/history
 
 echo "**************************************"
 echo "Starting MapReduce"
 echo "**************************************"
 
-VARIABLE_SET=`cat /etc/environment | grep HADOOP_MAPRED_HOME | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "HADOOP_MAPRED_HOME=/usr/lib/hadoop-mapreduce" >> /etc/environment
-fi
+echo "HADOOP_MAPRED_HOME=/usr/lib/hadoop-mapreduce" >> /etc/environment
+echo "YARN_HOME=/usr/lib/hadoop-yarn" >> /etc/environment
+echo "HADOOP_HDFS_HOME=/usr/lib/hadoop-hdfs" >> /etc/environment
+echo "HADOOP_COMMON_HOME=/usr/lib/hadoop" >> /etc/environment
+echo "HADOOP_CONF_DIR=/etc/hadoop/conf" >> /etc/environment
+echo "YARN_CONF_DIR=/etc/hadoop/conf" >> /etc/environment
 
-VARIABLE_SET=`cat /etc/environment | grep YARN_HOME | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "YARN_HOME=/usr/lib/hadoop-yarn" >> /etc/environment
-fi
+export HADOOP_MAPRED_HOME=/usr/lib/hadoop-mapreduce
+export YARN_HOME=/usr/lib/hadoop-yarn
+export HADOOP_HDFS_HOME=/usr/lib/hadoop-hdfs
+export HADOOP_COMMON_HOME=/usr/lib/hadoop
+export HADOOP_CONF_DIR=/etc/hadoop/conf
+export YARN_CONF_DIR=/etc/hadoop/conf
 
-VARIABLE_SET=`cat /etc/environment | grep HADOOP_HDFS_HOME | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "HADOOP_HDFS_HOME=/usr/lib/hadoop-hdfs" >> /etc/environment
-fi
+service hadoop-yarn-resourcemanager start
+service hadoop-yarn-nodemanager start
+service hadoop-mapreduce-historyserver start
 
-VARIABLE_SET=`cat /etc/environment | grep HADOOP_COMMON_HOME | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "HADOOP_COMMON_HOME=/usr/lib/hadoop" >> /etc/environment
-fi
+echo "**************************************"
+echo "Installing Hadoop ecosystem"
+echo "**************************************"
 
-VARIABLE_SET=`cat /etc/environment | grep HADOOP_CONF_DIR | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "HADOOP_CONF_DIR=/etc/hadoop/conf" >> /etc/environment
-fi
+apt-get install -y sqoop
+apt-get install -y pig
+apt-get install -y hive
 
-VARIABLE_SET=`cat /etc/environment | grep YARN_CONF_DIR | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "YARN_CONF_DIR=/etc/hadoop/conf" >> /etc/environment
-fi
+echo "**************************************"
+echo "Install MySQL"
+echo "**************************************"
 
+debconf-set-selections << EOF
+mysql-server-5.5 mysql-server/root_password password root
+EOF
 
-sudo service hadoop-yarn-resourcemanager start
-sudo service hadoop-yarn-nodemanager start
-sudo service hadoop-mapreduce-historyserver start
+debconf-set-selections << EOF
+mysql-server-5.5 mysql-server/root_password_again password root
+EOF
+
+apt-get install -y mysql-server-5.5
+apt-get install -y mysql-client-core-5.5
+chkconfig mysql on
+
+# Bind to external address
+sed -i 's/127\.0\.0\.1/'${VAGRANT_ETH1_IP}'/' /etc/mysql/my.cnf
+service mysql restart
+
+echo "**************************************"
+echo "Create Hive Metastore Schema"
+echo "**************************************"
+
+mysql -uroot -proot << EOF
+CREATE USER 'hive'@'${VAGRANT_ETH1_IP}' IDENTIFIED BY 'hive';
+CREATE DATABASE metastore;
+GRANT ALL ON metastore.* TO 'hive'@'${VAGRANT_ETH1_IP}';
+FLUSH PRIVILEGES;
+USE metastore;
+SOURCE /usr/lib/hive/scripts/metastore/upgrade/mysql/hive-schema-0.10.0.mysql.sql;
+EOF
+
+echo "**************************************"
+echo "Download / Install MySQL Driver"
+echo "**************************************"
+
+wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.31.tar.gz
+gunzip mysql-connector-java-5.1.31.tar.gz
+tar xf mysql-connector-java-5.1.31.tar
+JAR=mysql-connector-java-5.1.31/mysql-connector-java-5.1.31-bin.jar
+cp $JAR /usr/lib/sqoop/lib/mysql-connector-java.jar
+cp $JAR /usr/lib/hive/lib/mysql-connector-java.jar
+rm -rf mysql-connector-java-5.1.31*
+
+echo "**************************************"
+echo "Set Hive configuration"
+echo "**************************************"
+
+cp /vagrant/tmpl/hive-site.xml /etc/hive/conf/hive-site.xml
+sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hive/conf/hive-site.xml
+sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hive/conf/hive-site.xml
+cat /etc/hive/conf/hive-site.xml
+
+echo "**************************************"
+echo "Creating Hive HDFS structure"
+echo "**************************************"
+
+-u hdfs hadoop fs -mkdir /user/hive
+-u hdfs hadoop fs -mkdir /user/hive/warehouse
+
+-u hdfs hadoop fs -chown hive /user/hive/warehouse
+-u hdfs hadoop fs -chmod -R 1777 /user/hive/warehouse
+
+echo "**************************************"
+echo "Install Hive Server + Metastore"
+echo "**************************************"
+
+apt-get install -y hive-metastore
+apt-get install -y hive-server2
 
 echo "**************************************"
 echo "Install Hbase / Zookeeper packages"
 echo "**************************************"
 
-sudo apt-get install -y zookeeper
-sudo apt-get install -y zookeeper-server
-sudo apt-get install -y hbase-master
-sudo apt-get install -y hbase-regionserver
-sudo apt-get install -y hbase-thrift
-sudo apt-get install -y hbase-rest
+apt-get install -y zookeeper
+apt-get install -y zookeeper-server
+apt-get install -y hbase-master
+apt-get install -y hbase-regionserver
+apt-get install -y hbase-thrift
+apt-get install -y hbase-rest
 
 echo "**************************************"
 echo "Set HBase configuration"
 echo "**************************************"
 
-sudo cp /vagrant/tmpl/hbase-site.xml /etc/hbase/conf/hbase-site.xml
-sudo sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hbase/conf/hbase-site.xml
-sudo sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hbase/conf/hbase-site.xml
-cat /etc/hbase/conf/hbase-site.xml
+cp /vagrant/tmpl/hbase-site.xml /etc/hbase/conf/hbase-site.xml
+sed -i 's/VAGRANT_ETH1_IP/'${VAGRANT_ETH1_IP}'/g' /etc/hbase/conf/hbase-site.xml
+sed -i 's/VAGRANT_HOST/'${VAGRANT_HOST}'/g' /etc/hbase/conf/hbase-site.xml
 
-VARIABLE_SET=`cat /etc/environment | grep ZOO_LOG_DIR | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "ZOO_LOG_DIR=/var/log/zookeeper" >> /etc/environment
-fi
+echo "ZOO_LOG_DIR=/var/log/zookeeper" >> /etc/environment
+export ZOO_LOG_DIR=/var/log/zookeeper
 
-sudo service zookeeper-server init --myid=1
-sudo service zookeeper-server start
+service zookeeper-server init --myid=1
+service zookeeper-server start
 
 # Issue with variable overridden by zookeeper env
-sudo mv /etc/zookeeper/conf/zookeeper-env.sh /etc/zookeeper/conf/zookeeper-env.sh.bak
+mv /etc/zookeeper/conf/zookeeper-env.sh /etc/zookeeper/conf/zookeeper-env.sh.bak
 
 echo "**************************************"
 echo "Creating HBase HDFS structure"
 echo "**************************************"
 
-sudo -u hdfs hadoop fs -mkdir -p /user/hbase
-sudo -u hdfs hadoop fs -chown hbase /user/hbase
-sudo service hbase-master restart
-sudo service hbase-regionserver restart
+-u hdfs hadoop fs -mkdir -p /user/hbase
+-u hdfs hadoop fs -chown hbase /user/hbase
+service hbase-master restart
+service hbase-regionserver restart
 
 echo "**************************************"
 echo "Installing scala / spark"
 echo "**************************************"
 
 wget http://www.scala-lang.org/files/archive/scala-2.10.1.tgz
-sudo tar xvf scala-2.10.1.tgz
-sudo mv scala-2.10.1 /usr/lib
-sudo rm scala-2.10.1.tgz
-sudo ln -s /usr/lib/scala-2.10.1/ /usr/lib/scala
+tar xvf scala-2.10.1.tgz
+mv scala-2.10.1 /usr/lib
+rm scala-2.10.1.tgz
+ln -s /usr/lib/scala-2.10.1/ /usr/lib/scala
 
 wget http://d3kbcqa49mib13.cloudfront.net/spark-1.1.0-bin-hadoop2.4.tgz
-sudo tar -zxf spark-1.1.0-bin-hadoop2.4.tgz
-sudo mv spark-1.1.0-bin-hadoop2.4 /usr/lib
-sudo rm spark-1.1.0-bin-hadoop2.4.tgz
-sudo ln -s /usr/lib/spark-1.1.0-bin-hadoop2.4 /usr/lib/spark
+tar -zxf spark-1.1.0-bin-hadoop2.4.tgz
+mv spark-1.1.0-bin-hadoop2.4 /usr/lib
+rm spark-1.1.0-bin-hadoop2.4.tgz
+ln -s /usr/lib/spark-1.1.0-bin-hadoop2.4 /usr/lib/spark
 
-VARIABLE_SET=`cat /etc/environment | grep SCALA_HOME | grep -v PATH | wc -l`
-if [ $VARIABLE_SET -eq 0 ] ; then
-  echo "SCALA_HOME=/usr/lib/scala" >> /etc/environment
-fi
+CPU=`cat /proc/cpuinfo | grep processor | wc -l`
+
+touch /usr/lib/spark/conf/spark-env.sh
+chmod +x /usr/lib/spark/conf/spark-env.sh
+cat > /usr/lib/spark/conf/spark-env.sh <<EOF
+#!/usr/bin/env bash
+
+SPARK_WORKER_INSTANCES=1
+SPARK_WORKER_CORES=$CPU
+EOF
+
+cat > /usr/lib/spark/conf/spark-defaults.conf <<EOF
+spark.master			spark://vagrant:7077
+spark.eventLog.enabled		true
+spark.eventLog.dir		file:///tmp/spark-logs
+spark.serializer		org.apache.spark.serializer.KryoSerializer
+EOF
+
+echo 'PATH=${PATH}:/usr/lib/spark/bin >> /etc/environment'
+echo 'SCALA_HOME=/usr/lib/scala' >> /etc/environment
+export SCALA_HOME=/usr/lib/scala
+
+echo "**************************************"
+echo "Start Spark daemons"
+echo "**************************************"
+
+/usr/lib/spark/sbin/start-master.sh
+/usr/lib/spark/sbin/start-slaves.sh
 
 echo "**************************************"
 echo "Installing ElasticSearch"
 echo "**************************************"
 
-sudo apt-get install openjdk-7-jre-headless -y
 wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.1.1.deb
-sudo dpkg -i elasticsearch-1.1.1.deb
-sudo rm elasticsearch-1.1.1.deb
-sudo service elasticsearch start
-sudo update-rc.d elasticsearch defaults 95 10
+dpkg -i elasticsearch-1.1.1.deb
+rm elasticsearch-1.1.1.deb
+service elasticsearch start
+update-rc.d elasticsearch defaults 95 10
 
 echo "**************************************"
 echo "Installing Kibana"
 echo "**************************************"
 
-sudo apt-get install apache2 -y
-sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/kibana
-sudo mkdir -p /var/www/kibana
+apt-get install apache2 -y
+cp /etc/apache2/sites-available/default /etc/apache2/sites-available/kibana
+mkdir -p /var/www/kibana
 wget http://download.elasticsearch.org/kibana/kibana/kibana-latest.zip
 unzip kibana-latest.zip
-sudo rm kibana-latest.zip
-sudo mv kibana-latest/* /var/www/kibana
-sudo rm -r kibana-latest
-sudo a2ensite kibana
-sudo /etc/init.d/apache2 restart
+rm kibana-latest.zip
+mv kibana-latest/* /var/www/kibana
+rm -r kibana-latest
+a2ensite kibana
+/etc/init.d/apache2 restart
 
 echo "**************************************"
 echo "All done !"
 echo "**************************************"
+
 
